577576e1cb17c7102f9e58043f6bff29580c0696
SeniorTomatos <dehuckoo5@gmail.com>
2023-09-17 20:48:09 +0300

    Task 1.1.4 ready for check

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService service = new UserServiceImpl();
+        service.createUsersTable();
+        service.createUsersTable();
+        service.saveUser("Denis", "Strukov", (byte) 30);
+        service.saveUser("Pavel", "Seregin", (byte) 29);
+        service.removeUserById(1);
+        service.getAllUsers();
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,40 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+    UserDao dao = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        dao.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        dao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = dao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        dao.cleanUsersTable();
     }
 }
